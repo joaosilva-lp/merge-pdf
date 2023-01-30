565fce62c8ab4013b6d3e0d9f146457ede41f48b
João Silva <99473819+joaosilva-lp@users.noreply.github.com>
2023-01-30 16:17:22 +0000

    Add files via upload

---
+++ juntaPDFs.js
@@ -0,0 +1,20 @@
+﻿const fs = require('fs');
+const PDFMerger = require('pdf-merger-js');
+
+(async () => {
+  const merger = new PDFMerger();
+
+  fs.readdir('./', async (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      if (file.endsWith('.pdf')) {
+        await merger.add(file);
+      }
+    }
+
+    await merger.save('merged.pdf');
+  });
+})();
+
+console.log('Programa Concluído');
